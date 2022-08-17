07d2717731a132a7256e1b6306c6dfec559a87d2
Mojtaba Mohammadi <102634674+mojmhmdi@users.noreply.github.com>
2022-08-17 16:37:08 +0430

    Update GPU_TEST_pytorch.py

--- GPU_TEST_pytorch.py
+++ GPU_TEST_pytorch.py
@@ -5,7 +5,7 @@ import time
 import os
 # for cpu,uncomment line 8, comment line 18 and uncomment line 19
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # check wether kernel detects your GPU
 torch.cuda.is_available()
@@ -14,8 +14,8 @@ cuda = torch.device('cuda')  # Default CUDA device
 al = time.time()
 for i in range(150):
     # for cpu just delet device =  cuda
-    # torch.randn((100000, 4000), device=cuda)  # for GPU
-    torch.randn((100000, 200))  # for CPU
+    torch.randn((100000, 4000), device=cuda)  # for GPU
+    #torch.randn((100000, 200))  # for CPU
 
 b = time.time()
 print(f'your run time is: {b-al}', 's')
